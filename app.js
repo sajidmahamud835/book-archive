8bbc050903bd46f9c3fa499520c36d1dfacec64c
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-09-02 19:31:51 +0600

    javascript connected

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('javasrcipt is connected');
+
+/* -------------------------------
+    Global variable and selectors
+--------------------------------- */
+
+//error messages
+const resultFound = document.getElementById('result-found');
+const emptyInput = document.getElementById('empty-input');
+const noResult = document.getElementById('no-result');
+const error = document.getElementById('error');
