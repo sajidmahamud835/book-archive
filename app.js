736b91c28a634bdab09c211d0478c9f212d1df58
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-09-02 19:41:34 +0600

    event listener added

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-console.log('javasrcipt is connected');
+console.log('javasrcipt is connected'); //test if javascript is connected
 
 /* -------------------------------
     Global variable and selectors
 --------------------------------- */
+
+//search box
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
 
 //error messages
 const resultFound = document.getElementById('result-found');
 const emptyInput = document.getElementById('empty-input');
 const noResult = document.getElementById('no-result');
 const error = document.getElementById('error');
+
+
+/* --------------------
+    Event listener
+---------------------- */
+
+//search button
+searchButton.addEventListener('click', () => console.log('button clicked'));
