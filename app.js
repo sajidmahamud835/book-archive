b6b06bf04ece682f4114221df92549cbf32a0912
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-09-02 20:07:22 +0600

    search book api added

--- app.js
+++ app.js
@@ -14,10 +14,21 @@ const emptyInput = document.getElementById('empty-input');
 const noResult = document.getElementById('no-result');
 const error = document.getElementById('error');
 
+/* ---------------
+    API call
+-----------------*/
+//search books
+const getBooks = kyeword => {
+    console.log(kyeword); //test if kyeword is passed correctly.
+    fetch(`https://openlibrary.org/search.json?q=${kyeword}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+};
+
 
 /* --------------------
     Event listener
 ---------------------- */
 
 //search button
-searchButton.addEventListener('click', () => console.log('button clicked'));
+searchButton.addEventListener('click', () => getBooks(searchInput.value));
