51fd5e518e2d2f5192ac053d7d4831ec2a2dc261
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-09-02 20:44:04 +0600

    static element created using forEach

--- app.js
+++ app.js
@@ -14,6 +14,45 @@ const emptyInput = document.getElementById('empty-input');
 const noResult = document.getElementById('no-result');
 const error = document.getElementById('error');
 
+//result container
+const resultContainer = document.getElementById('result-container');
+
+
+/* -----------------
+    Create results
+------------------- */
+const createResults = data => {
+    data.docs.forEach(result => {
+        console.log('creating result.....')
+        const resultCard = document.createElement('article');
+        resultCard.innerHTML = `
+        <article class="col shadow rounded ms-4">
+    
+            <div class="card border-0 h-100">
+    
+                <div class="card-body py-1">
+                    <h5 class="card-title">Things Go Wrong For Me</h5>
+                    <img src="https://covers.openlibrary.org/b/id/9055807-M.jpg" class="card-img" alt="...">
+                    <p class="card-text">
+                    <small class="d-block"><span class="text-primary">Author:</span> Rodney Lacroix</small>
+                    <small class="d-block"><span class="text-primary">Publisher:</span> Progamming
+                        Hero</small>
+                    </p>
+                </div>
+    
+                <div class="card-footer py-1">
+                    <small class="text-muted">First published in 2012.</small>
+                </div>
+    
+            </div> 
+    
+        </article>
+        `;
+        resultContainer.appendChild(resultCard);
+    });
+}
+
+
 /* ---------------
     API call
 -----------------*/
@@ -22,7 +61,7 @@ const getBooks = kyeword => {
     console.log(kyeword); //test if kyeword is passed correctly.
     fetch(`https://openlibrary.org/search.json?q=${kyeword}`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => createResults(data))
 };
 
 
